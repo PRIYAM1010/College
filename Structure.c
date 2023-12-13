0f1902ec4b88a0548da91bad28122babfe9c7073
PRIYAM1010 <152970347+PRIYAM1010@users.noreply.github.com>
2023-12-13 18:46:23 +0530

    Create Structure.c

---
+++ Structure.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<string.h>
+struct Details{
+        int rollno;
+        char name[50],gender[50];
+    }s1,s2,s3,s4,s5,s6,s7,s8,s9,s10;
+    int main()
+    {
+        
+        s1.rollno=1;
+        strcpy(s1.name,"Priyam Saxena");
+        strcpy(s1.gender,"Male");
+        printf("Student's Roll No: %d\n",s1.rollno);
+        printf("Student's Name: %s\n",s1.name);
+        printf("Student's gender: %s\n",s1.gender);
+        s2.rollno=2;
+        strcpy(s2.name,"Durgesh Tewary");
+        strcpy(s2.gender,"Male");
+        printf("Student's Roll No: %d\n",s2.rollno);
+        printf("Student's Name: %s\n",s2.name);
+        printf("Student's gender: %s\n",s2.gender);
+        s3.rollno=3;
+        strcpy(s3.name,"Nishant Agarwal");
+        strcpy(s3.gender,"Male");
+        printf("Student's Roll No: %d\n",s3.rollno);
+        printf("Student's Name: %s\n",s3.name);
+        printf("Student's gender: %s\n",s3.gender);
+        s4.rollno=4;
+        strcpy(s4.name,"Ridhi Sud");
+        strcpy(s4.gender,"Female");
+        printf("Student's Roll No: %d\n",s4.rollno);
+        printf("Student's Name: %s\n",s4.name);
+        printf("Student's gender: %s\n",s4.gender);
+        s5.rollno=5;
+        strcpy(s5.name,"Saket Maurya");
+        strcpy(s5.gender,"Male");
+        printf("Student's Roll No: %d\n",s5.rollno);
+        printf("Student's Name: %s\n",s5.name);
+        printf("Student's gender: %s\n",s5.gender);
+        s6.rollno=6;
+        strcpy(s6.name,"Arpit Garg");
+        strcpy(s6.gender,"Male");
+        printf("Student's Roll No: %d\n",s6.rollno);
+        printf("Student's Name: %s\n",s6.name);
+        printf("Student's gender: %s\n",s6.gender);
+        s7.rollno=7;
+        strcpy(s7.name,"Rohit");
+        strcpy(s7.gender,"Male");
+        printf("Student's Roll No: %d\n",s7.rollno);
+        printf("Student's Name: %s\n",s7.name);
+        printf("Student's gender: %s\n",s7.gender);
+        s8.rollno=8;
+        strcpy(s8.name,"Ridhima Singh");
+        strcpy(s8.gender,"Female");
+        printf("Student's Roll No: %d\n",s8.rollno);
+        printf("Student's Name: %s\n",s8.name);
+        printf("Student's gender: %s\n",s8.gender);
+        s9.rollno=9;
+        strcpy(s9.name,"Neha Kharwar");
+        strcpy(s9.gender,"Female");
+        printf("Student's Roll No: %d\n",s9.rollno);
+        printf("Student's Name: %s\n",s9.name);
+        printf("Student's gender: %s\n",s9.gender);
+        s10.rollno=10;
+        strcpy(s10.name,"Divyanshu Roy");
+        strcpy(s10.gender,"Male");
+        printf("Student's Roll No: %d\n",s10.rollno);
+        printf("Student's Name: %s\n",s10.name);
+        printf("Student's gender: %s\n",s10.gender);
+        
+    }
