7f85a362066bd334c67b467f1931e19fca52e62b
PRIYAM1010 <152970347+PRIYAM1010@users.noreply.github.com>
2023-12-25 23:27:54 +0530

    A2 q10.c

---
+++ A2 q10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int *x,int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+
+}
+int main()
+{
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d,%d",&a,&b);
+    swap(&a,&b);
+    printf("%d\n",a);
+    printf("%d",b);
+    return 0;
+}
